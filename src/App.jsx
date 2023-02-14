6d7574dedfb28cf9b6f8abe27deb0b9da1a07a43
thapa technical <vbthapa55@gmail.com>
2023-02-14 15:02:53 +0530

    react table mini project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import UserData from "./components/UserData.jsx";
+
+const API = "https://jsonplaceholder.typicode.com/users";
+
+const App = () => {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            if (data.length > 0) {
+                setUsers(data);
+            }
+            console.log(data);
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+
+    useEffect(() => {
+        fetchUsers(API);
+    }, [])
+    return <>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+            </tr>
+            </thead>
+            <tbody>
+            <UserData users={users}/>
+            </tbody>
+        </table>
+    </>
+}
+
+export default App;
+
